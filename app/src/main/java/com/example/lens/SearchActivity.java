8b7c5cde06031aae466d9f849681ab155dfe5a68
유림 이 <dbfla3268@gmail.com>
2023-03-02 18:43:55 +0900

    listview challenge

--- app/src/main/java/com/example/lens/SearchActivity.java
+++ app/src/main/java/com/example/lens/SearchActivity.java
@@ -25,8 +25,8 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search2);
 
 
-        final String[] mid = {"히어로즈", "24시", "로스트", "로스트룸", "스몰빌", "탐정몽크", "빅뱅이론",
-                "프렌즈", "덱스터", "글리", "가쉽걸", "테이큰", "슈퍼내추럴", "브이"};
+        final String[] mid = {"컬러렌즈", "렌즈액", "렌즈액", "렌즈액", "렌즈액", "렌즈액", "렌즈액",
+                };
 
         ListView list = (ListView) findViewById(R.id.listView);
 
